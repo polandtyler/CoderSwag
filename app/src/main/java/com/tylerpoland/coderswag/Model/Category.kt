131f69a175d1a7915512cd18d1a46c258b287af3
Tyler Poland <poland.tyler@gmail.com>
2018-09-04 12:38:20 -0600

    adapter + listview

--- app/src/main/java/com/tylerpoland/coderswag/Model/Category.kt
+++ app/src/main/java/com/tylerpoland/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.tylerpoland.coderswag.Model
 
-class Category(val name: String, val image: String)
+class Category(val name: String, val image: String) {
+    override fun toString(): String {
+        return name
+    }
+}
